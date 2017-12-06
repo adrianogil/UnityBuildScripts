1b199b1c37236f68d7a24ec1bc78be346f8a8d4c
Adriano Gil <adrianogil.san@gmail.com>
2017-12-06 17:29:53 -0400

    Removed android (non Unity related) functions

--- unity_build_bashrc.sh
+++ unity_build_bashrc.sh
@@ -35,9 +35,6 @@ alias 2dotg='echo "Unity 2D Build and Run" && ../tools/run2dOTG_and_install.sh'
 # Test
 alias py-unity-build='python '${UNITY_BUILD_SCRIPTS_DIR}'/unity_build.py'
 
-# Android
-alias gilcat='adb log
-cat | grep GilLog'
 
 # Dynamic Unity versions alias
 for file in /Applications/Unity*/ ; do
@@ -48,54 +45,6 @@ for file in /Applications/Unity*/ ; do
     alias open-unity-${base_file}="${file}/Unity${base_file}.app/Contents/MacOS/Unity -projectPath \$PWD"
   fi;
 done
-
-# Install Android APK
-function ik()
-{
-    echo 'Searching for APK files ...'
-
-    apk_file=$(find . -name '*.apk' | head -1)
-
-    if [ -z $apk_file ]; then
-        echo 'No APK Found!'
-    else
-        echo 'Found '$apk_file
-        adb install -r $apk_file
-    fi
-}
-
-# Android logcat
-function dlog()
-{
-    device_model=$(adb shell getprop ro.product.model)
-    echo "Device is $device_model"
-    log_file=log_${device_model}_$(date +%F-%H:%M).txt
-    echo 'Android log saved as '$log_file
-    adb shell logcat -d -v time > $log_file
-    number_of_lines=$(cat $log_file | wc -l)
-    echo ''$number_of_lines' lines'
-}
-
-# Cat last logcat saved by dlog
-alias getlog='ls -t log_*.txt | head -1'
-alias catlog='ls -t log_*.txt | head -1 | xargs -I {} cat {}'
-alias openlog='ls -t log_*.txt | head -1 | xargs -I {} sublime {}'
-
-function logtext() {
-    ls -t log_*.txt | head -1 | xargs -I {} cat {} | grep $1 | less
-}
-
-function catexception()
-{
-    ls -t log_*.txt | head -1 | xargs -I {} cat {} | python ${UNITY_BUILD_SCRIPTS_DIR}/android/log/error_log_filter.py
-}
-
-function logexception()
-{
-    catexception | less
-}
-
-
 
 # Extract JAR from AAR files in current directory
 alias extract-jar-from='ls *.aar | rev | cut -c5- | rev | xargs -I {}  $UNITY_BUILD_SCRIPTS_DIR/android/aar/extract_jar_from_aar.sh {}'
